3738e02209f009d2a81111f447867a1991ce818a
Blaine Gunn <blainegunn@Black-Mac.local>
2019-06-23 22:48:06 -0600

    is/are logic

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -16,9 +16,11 @@ class TagRoute extends React.Component {
     const tag = this.props.pageContext.tag
     const title = this.props.data.site.siteMetadata.title
     const totalCount = this.props.data.allMarkdownRemark.totalCount
-    const tagHeader = `${totalCount} post${
+    const tagHeader = `There ${
+      totalCount === 1 ? 'is' : 'are'
+    } ${totalCount} post${
       totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`
+    } tagged ${tag}.`
 
     return (
       <Layout>
@@ -32,7 +34,7 @@ class TagRoute extends React.Component {
               >
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
                 <ul className="taglist taglist-big">{postLinks}</ul>
-                <p>
+                <p className="link">
                   <Link to="/tags/">Browse all tags</Link>
                 </p>
               </div>
